f2ec8be1f69200f0da07e0586b485a7b75af5959
Markiian Karpa <markian98@gmail.com>
2022-07-10 23:05:12 +0200

    Create ProgressSection.svelte

---
+++ src/lib/components/ProgressSection.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import Button from './Button.svelte';
+  import Progress from './Progress.svelte';
+
+  function tryAgainClicked(): void {
+    window.location.reload();
+  }
+
+  function nextClicked(): void {
+    console.log("nextClicked");
+  }
+</script>
+
+<div class="progress-section">
+  <Progress/>
+  <div class="buttons">
+    <Button color="#facd34" text="Try again" on:click={tryAgainClicked} />
+    <Button color="#34bafa" text="Next" on:click={nextClicked} />
+  </div>
+</div>
+
+<style>
+  .progress-section {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .buttons {
+    margin-top: 1.25rem;
+  }
+</style>

4a1b1bb652151f139873ca8524c248bdd07bdf97
Markiian Karpa <markian98@gmail.com>
2022-07-10 23:11:04 +0200

    Create ConfirmButton.svelte

---
+++ src/lib/components/ConfirmButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  function handleClick() {
+    dispatch('clicked');
+	}
+</script>
+
+<button class='confirm' on:click={handleClick}>
+  Confirm
+</button>
+
+<style>
+  .confirm {
+    background-color: #007ACC;
+    color: white;
+    border: none;
+    width: 100%;
+    height: 2rem;
+    position: absolute;
+    bottom: 0;
+    cursor: pointer;
+  }
+</style>
